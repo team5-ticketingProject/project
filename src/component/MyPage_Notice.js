cf0298c74b7e2c26ccaac5bd738f30c76ee14087
JangHaeMin <asaw2123@naver.com>
2023-10-19 10:53:33 +0900

    Notice_Content commit

--- src/component/MyPage_Notice.js
+++ src/component/MyPage_Notice.js
@@ -3,10 +3,10 @@ import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import axios from "axios";
 
-
 function Notice() {
   const [notices, setNotices] = useState([]); // 공지사항 목록
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const [selectedNotice, setSelectedNotice] = useState(null); // 선택한 공지사항
   const noticesPerPage = 10; // 한 페이지에 보여질 공지사항 수
 
   // 예시로 공지사항 데이터를 상태에 추가
@@ -15,7 +15,6 @@ function Notice() {
       .get("http://localhost:5000/getNotice")
       .then((response) => {
         setNotices(response.data);
-        console.log("Notices:", notices);
       })
       .catch((error) => {
         console.error(error);
@@ -30,6 +29,15 @@ function Notice() {
   const indexOfFirstNotice = indexOfLastNotice - noticesPerPage;
   const currentNotices = notices.slice(indexOfFirstNotice, indexOfLastNotice);
 
+  const handleNoticeClick = (notice) => {
+    setSelectedNotice(notice);
+  };
+
+  // 모달 닫기
+  const closeNoticeModal = () => {
+    setSelectedNotice(null);
+  };
+
   return (
     <div className="Notice">
       <div style={{ borderBottom: "2px solid #ccc" }}>
@@ -39,7 +47,7 @@ function Notice() {
       </div>
       <div style={{ marginTop: "20px" }}>
         <h4>
-          최신 <span style={{color:"red"}}>공지사항</span>을 확인하실 수 있습니다.
+          최신 <span style={{ color: "red" }}>공지사항</span>을 확인하실 수 있습니다.
           <br />
           자세한 내용은 해당 공지사항을 클릭하여 확인하세요.
         </h4>
@@ -49,21 +57,32 @@ function Notice() {
           <thead>
             <tr>
               <th>제목</th>
-              <th>내용</th>
               <th>작성일</th>
             </tr>
           </thead>
           <tbody>
             {currentNotices.map((notice, index) => (
-              <tr key={index}>
+              <tr key={index} onClick={() => handleNoticeClick(notice)}>
                 <td>{notice.title}</td>
-                <td>{notice.content}</td>
                 <td> {new Date(notice.date).toISOString().split("T")[0]}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+      {selectedNotice && (
+        <div className="Modal">
+          <div className="ModalContent">
+            <span className="CloseButton" onClick={closeNoticeModal}>
+              &times;
+            </span>
+            <h3>{selectedNotice.title}</h3>
+            <p>{selectedNotice.content}</p>
+          </div>
+        </div>
+      )
+      }
+
       <div className="Pagination">
         <Stack spacing={2}>
           <Pagination
