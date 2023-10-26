efdb9472d1d0548e8519df046d20c08dac0323db
JangHaeMin <asaw2123@naver.com>
2023-10-26 15:30:36 +0900

    pw_change label commit
    htmlFor로 교체

--- src/component/Pw_Change.js
+++ src/component/Pw_Change.js
@@ -6,25 +6,25 @@ function PwChange() {
     <form className="ContactForm"> 
       
       <div>
-        <label for="currentPassword">현재 비밀번호:</label>
+        <label htmlFor="currentPassword">현재 비밀번호:</label>
         <input type="password" id="currentPassword" name="currentPassword" required  className="input-field" />
       </div>
 
     
       <div>
-        <label for="newPassword">새로운 비밀번호:</label>
+        <label htmlFor="newPassword">새로운 비밀번호:</label>
         <input type="password" id="newPassword" name="newPassword" required  className="input-field" />
       </div>
 
      
       <div>
-        <label for="confirmNewPassword">새로운 비밀번호 확인:</label>
+        <label htmlFor="confirmNewPassword">새로운 비밀번호 확인:</label>
         <input type="password" id="confirmNewPassword" name="confirmNewPassword" required  className="input-field" />
       </div>
 
       
       <div>
-        <button type="submit"  className="submit-button">비밀번호 변경</button>
+        <button htmlFor="submit"  className="submit-button">비밀번호 변경</button>
       </div>
     </form>
   );
