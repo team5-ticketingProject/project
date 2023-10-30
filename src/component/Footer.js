f87988638d5346b8a37afea5bfcea557b1913873
JangHaeMin <asaw2123@naver.com>
2023-10-30 13:11:12 +0900

    remove footer commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,10 +1,8 @@
 import '../css/Footer.css';
-import MacAddressCollector from './MacAddressCollector';
 
 const Footer = () => {
     return(
         <div className="footer">
-        <MacAddressCollector />
             footer영역
         </div>
     );
